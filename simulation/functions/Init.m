8783852f809d4ae2a14f9c1b5d3dd5e8eebb694f
Christian Funck <cfunck@ethz.ch>
2013-04-11 22:17:04 +0200

    deleted unused files

--- simulation/functions/Init.m
+++
@@ -1,5 +0,0 @@
-%% Initializing Process
-%  Update directories, set root directory
-
-global root;
-root = 'C:\Users\matthias\Documents\ETH\ITET\Gruppenarbeit\';
--- simulation/functions/getDirStructure.m
+++
@@ -1,8 +0,0 @@
-function DirList = getDirStructure(relPath)
-    global root
-    list = dir([root relPath]);
-    DirList = cell(length(list)-2,1);
-    for i=3:length(list)
-        DirList(i-2) = cellstr(list(i).name);
-    end
-end
